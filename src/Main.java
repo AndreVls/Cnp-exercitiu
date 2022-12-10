4c73224fa1066243154513cda6ae28ad9db18fde
AndreVls <tampuandreea@yahoo.com>
2022-12-10 11:18:32 +0200

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        String Cnp  = "286112908037";
+        if ( Cnp.length()!= 13) {
+            System.out.println("Cnp not valid");
+        }
+        String[] CnpFormatted = Cnp = Cnp split(/)
+
+
+    }
+}
+
+
